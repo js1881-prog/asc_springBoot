c2e53781eb1a8969500180a5a63c438ad88055c1
padonan <bbde1861@gmail.com>
2022-11-02 21:16:18 +0900

    11 02 초기 프로젝트 생성 jdk 11

---
+++ src/main/java/asc/portfolio/ascSb/AscSbApplication.java
@@ -0,0 +1,13 @@
+package asc.portfolio.ascSb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AscSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asc/portfolio/ascSb/AscSbApplicationTests.java
@@ -0,0 +1,13 @@
+package asc.portfolio.ascSb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
