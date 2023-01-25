8bfa4616ca38cbeb02dd84ab8ce87cf7e8a8f70f
ch-yang <ych1273@gmail.com>
2023-01-25 16:34:02 +0900

    test: 업데이트 결과 확인 테스트 추가
    Fixed-Term Ticket (성공)
    Part-Time Ticket (실패) : 항상 좌석이 종료됨
    Start-Time (성공)

--- src/test/java/asc/portfolio/ascSb/domain/seat/SeatRepositoryTest.java
+++ src/test/java/asc/portfolio/ascSb/domain/seat/SeatRepositoryTest.java
@@ -1,13 +1,18 @@
 package asc.portfolio.ascSb.domain.seat;
 import asc.portfolio.ascSb.domain.cafe.Cafe;
 import asc.portfolio.ascSb.domain.cafe.CafeRepository;
+import asc.portfolio.ascSb.domain.product.ProductRepository;
+import asc.portfolio.ascSb.domain.seatreservationinfo.SeatReservationInfoRepository;
 import asc.portfolio.ascSb.domain.ticket.Ticket;
 import asc.portfolio.ascSb.domain.ticket.TicketRepository;
 import asc.portfolio.ascSb.domain.ticket.TicketStateType;
 import asc.portfolio.ascSb.domain.user.User;
 import asc.portfolio.ascSb.domain.user.UserRepository;
 import asc.portfolio.ascSb.domain.user.UserRoleType;
-import org.junit.jupiter.api.DisplayName;
+import asc.portfolio.ascSb.service.cafe.CafeService;
+import asc.portfolio.ascSb.service.seat.SeatService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +28,12 @@ public class SeatRepositoryTest {
     SeatRepository seatRepository;
 
     @Autowired
+    SeatReservationInfoRepository seatReservationInfoRepository;
+
+    @Autowired
+    SeatService seatService;
+
+    @Autowired
     UserRepository userRepository;
 
     @Autowired
@@ -31,17 +42,35 @@ public class SeatRepositoryTest {
     @Autowired
     CafeRepository cafeRepository;
 
-    private void setSeatTestData() {
+    @Autowired
+    CafeService cafeService;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    public void before() {
+        productRepository.deleteAllInBatch();
+        seatReservationInfoRepository.deleteAllInBatch();
+        ticketRepository.deleteAllInBatch();
+        seatRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+        cafeRepository.deleteAllInBatch();
+    }
+
+    private Cafe careateNewCafe(String cafeName) {
         Cafe cafe = Cafe.builder()
-                .cafeName("testData_서울")
+                .cafeName(cafeName)
                 .build();
 
-        cafeRepository.save(cafe);
+        return cafeRepository.saveAndFlush(cafe);
+    }
 
-        String loginId = "ascUser1234";
-        String password = "ascUser1234";
-        String email = "asc@gmail.com";
-        String name = "asc";
+    private User createNewUser(String prefix) {
+        String loginId = prefix + "1234";
+        String password = loginId;
+        String email = prefix + "@gmail.com";
+        String name = prefix;
 
         User user = User.builder()
                 .loginId(loginId)
@@ -51,31 +80,157 @@ public class SeatRepositoryTest {
                 .role(UserRoleType.USER)
                 .build();
 
-        userRepository.save(user);
+        return userRepository.saveAndFlush(user);
+    }
 
+    private Seat createNewSeat(Cafe cafe, int seatNumber) {
         Seat seat = Seat.builder()
-                .seatNumber(0)
+                .seatNumber(seatNumber)
                 .cafe(cafe)
                 .build();
 
-        LocalDateTime date = LocalDateTime.now();
+        return seatRepository.saveAndFlush(seat);
+    }
+
+    private Ticket createNewFixedTicket(Cafe cafe, User user, LocalDateTime endTime) {
         Ticket ticket = Ticket.builder()
                 .cafe(cafe)
                 .user(user)
                 .isValidTicket(TicketStateType.VALID)
                 .ticketPrice(3000)
-                .fixedTermTicket(date.plusSeconds(5))
+                .productLabel("FIXED-TERM" + 12123)
+                .fixedTermTicket(endTime)
                 .partTimeTicket(null)
                 .remainingTime(null)
                 .build();
 
-        seat.reserveSeat(user, ticket);
+        return ticketRepository.saveAndFlush(ticket);
+    }
 
-        seatRepository.save(seat);
+    private Ticket createNewPartTimeTicket(Cafe cafe, User user, Long remainingTime) {
+        Ticket ticket = Ticket.builder()
+                .cafe(cafe)
+                .user(user)
+                .isValidTicket(TicketStateType.VALID)
+                .ticketPrice(3000)
+                .productLabel("PART-TIME" + 12123)
+                .fixedTermTicket(null)
+                .partTimeTicket(60L)
+                .remainingTime(remainingTime)
+                .build();
+
+        return ticketRepository.saveAndFlush(ticket);
     }
 
     @Test
     public void Seat_좌석생성기() {
-        setSeatTestData();
+        Cafe cafe = careateNewCafe("testData_서울");
+        User user = createNewUser("ascUser");
+        Seat seat = createNewSeat(cafe, 0);
+        Ticket ticket = createNewFixedTicket(cafe, user, LocalDateTime.now().plusSeconds(5));
+
+        cafeService.changeReservedUserCafe(user, "testData_서울");
+
+        seatService.reserveSeat(user, 0, 4L);
+        seatRepository.saveAndFlush(seat);
+    }
+
+    @Test
+    public void Seat_Update_With_FixedTicket_0() {
+        Cafe cafe = careateNewCafe("testData_서울");
+        User user = createNewUser("ascUser");
+        Seat seat = createNewSeat(cafe, 0);
+        cafeService.changeReservedUserCafe(user, "testData_서울");
+
+        // 3 초 후 만료되는 FixedTicket 으로 좌석 예약
+        Ticket ticket = createNewFixedTicket(cafe, user, LocalDateTime.now().plusSeconds(3));
+
+        seatService.reserveSeat(user, 0, 4L);
+
+        // 4초 후 만료 여부 확인
+        sleepMillis(4000);
+        int count = seatService.updateAllReservedSeatState();
+        Assertions.assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void Seat_Update_With_FixedTicket_1() {
+        Cafe cafe = careateNewCafe("testData_서울");
+        User user = createNewUser("ascUser");
+        Seat seat = createNewSeat(cafe, 0);
+        cafeService.changeReservedUserCafe(user, "testData_서울");
+
+        // 100 초 후 만료되는 FixedTicket 으로 좌석 예약
+        Ticket ticket = createNewFixedTicket(cafe, user, LocalDateTime.now().plusSeconds(100));
+
+        seatService.reserveSeat(user, 0, 4L);
+
+        // 4초 후 만료 여부 확인
+        sleepMillis(4000);
+        int count = seatService.updateAllReservedSeatState();
+        Assertions.assertThat(count).isEqualTo(0);
+    }
+
+    @Test
+    public void Seat_Update_With_StartTime() {
+        Cafe cafe = careateNewCafe("testData_서울");
+        User user = createNewUser("ascUser");
+        Seat seat = createNewSeat(cafe, 0);
+
+        cafeService.changeReservedUserCafe(user, "testData_서울");
+        Ticket ticket = createNewFixedTicket(cafe, user, LocalDateTime.now().plusSeconds(100));
+
+        // 0시간 동안 좌석 예약 (만료 O)
+        seatService.reserveSeat(user, 0, 0L);
+        int count = seatService.updateAllReservedSeatState();
+        Assertions.assertThat(count).isEqualTo(1);
+
+        // 1시간 동안 좌석 예약 (만료 X)
+        seatService.reserveSeat(user, 0, 1L);
+        count = seatService.updateAllReservedSeatState();
+        Assertions.assertThat(count).isEqualTo(0);
+    }
+
+    @Test
+    public void Seat_Update_With_PartTimeTicket_종료X() {
+        Cafe cafe = careateNewCafe("testData_서울");
+        User user = createNewUser("ascUser");
+        Seat seat = createNewSeat(cafe, 0);
+        cafeService.changeReservedUserCafe(user, "testData_서울");
+
+        // 60 분 후 만료되는 PartTimeTicket 으로 좌석 예약
+        Ticket ticket = createNewPartTimeTicket(cafe, user, 60L);
+
+        seatService.reserveSeat(user, 0, 4L);
+
+        // 바로 만료 확인
+        int count = seatService.updateAllReservedSeatState();
+        Assertions.assertThat(count).isEqualTo(0);
+    }
+
+    @Test
+    public void Seat_Update_With_PartTimeTicket_종료O() {
+        Cafe cafe = careateNewCafe("testData_서울");
+        User user = createNewUser("ascUser");
+        Seat seat = createNewSeat(cafe, 0);
+        cafeService.changeReservedUserCafe(user, "testData_서울");
+
+        // 1 분 후 만료되는 PartTimeTicket 으로 좌석 예약
+        Ticket ticket = createNewPartTimeTicket(cafe, user, 1L);
+
+        seatService.reserveSeat(user, 0, 4L);
+
+        // 1 분 후 만료 확인
+        sleepMillis(65 * 1000);
+        int count = seatService.updateAllReservedSeatState();
+        Assertions.assertThat(count).isEqualTo(1);
+    }
+
+    private void sleepMillis(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("InterruptedException", e);
+        }
     }
 }
